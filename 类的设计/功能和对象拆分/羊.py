a4f2ada9eb857e78539ddd37b9677435d1a87f29
sxmeng2017 <570701164@qq.com>
2019-11-27 21:13:29 +0800

    __init___
    多继承init的调用问题

--- 类的设计/功能和对象拆分/羊.py
+++ 类的设计/功能和对象拆分/羊.py
@@ -1,8 +1,16 @@
+"""
+这里有个问题，就是__init__在多继承中，如果给予的变量已经能全部填给前面继承的类
+后面的继承的类的的__init__实际上不会被执行。
+所以如果要attack，run能对继承的子类有所检验，看样子最好还是在动作里检测
+或者子类里面检测。
+"""
+
 class Animal:
     """
     Animal类作为所有动物的抽象类。只提供最基本的是否为哺乳动物的属性和判断
     """
     def __init__(self, is_mammal=True):
+        print('ani')
         self.is_mammal = is_mammal
 
     def isMammal(self):
@@ -17,6 +25,7 @@ class AbstractSheep(Animal):
 
     def __init__(self, name, sex=None, horn=None, color=None, tail=None):
         super(AbstractSheep, self).__init__(is_mammal=self.is_mammal)
+        print('ab')
         self.name = name
         self.sex = sex
         self.horn = horn
@@ -28,6 +37,7 @@ class AbstractSheep(Animal):
 class Run:
 
     def __init__(self, speed=10, step=10):
+        print('run')
         self.name = None
         self.speed = speed
         self.step = step
@@ -35,14 +45,33 @@ class Run:
     def run(self):
         print('{} is running'.format(self.name))
 
+class Horn:
+
+    def __init__(self, length=None, hardness=None):
+        self.length = length
+        self.hardness = hardness
+
+class Attack:
+
+    def __init__(self):
+        #if self.horn:
+        print('{} can attack'.format(self.name))
+        #else:
+        #    raise ValueError('you dont have horns')
+
+    def attack(self):
+        print('Attack!')
 
 #******************************** 最上层 ************************************#
-class sheep(AbstractSheep, Run):
+class sheep(AbstractSheep, Run, Attack):
 
-    def __init__(self, name):
-        super(sheep, self).__init__(name)
+    def __init__(self, name, **kwargs):
+        super(sheep, self).__init__(name, **kwargs)
+        print('ok')
 
 
 if __name__ == '__main__':
-    s = sheep('sheep1')
+    h = Horn()
+    s = sheep('sheep0')
     s.run()
+    #s.attack()
